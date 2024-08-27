22aa6c5144784c247acf835b873053970dcfbb92
Matthieu Mota <matthieumota@gmail.com>
2024-08-27 19:29:20 +0200

    wip product manager

---
+++ tests/Admin/ProductControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests;
+
+use App\Factory\UserFactory;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class ProductControllerTest extends WebTestCase
+{
+    use Factories;
+    use ResetDatabase;
+
+    public function testAdminCanSeeAdminProducts(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne(['email' => 'fiorella@boxydev.com', 'roles' => ['ROLE_ADMIN']]);
+
+        // Act
+        $this->client->loginUser($user->_real());
+        $this->client->request('GET', '/admin/produits');
+
+        // Assert
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Produits');
+    }
+
+    public function testUserCannotSeeAdminProducts(): void
+    {
+        // Arrange
+        $user = UserFactory::createOne(['email' => 'fiorella@boxydev.com']);
+
+        // Act
+        $this->client->loginUser($user->_real());
+        $this->client->request('GET', '/admin/produits');
+
+        // Assert
+        $this->assertResponseStatusCodeSame(404);
+    }
+
+    public function testGuestCannotSeeAdminProducts(): void
+    {
+        // Act
+        $this->client->request('GET', '/admin/produits');
+
+        // Assert
+        $this->assertResponseStatusCodeSame(404);
+    }
+}
